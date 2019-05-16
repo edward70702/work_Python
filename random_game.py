8b5b94e48034453981a3b6089a77ed175e4ab339
edward70702 <edward70702@gmail.com>
2019-05-16 12:50:39 +0800

    random_game_V2

--- random_game.py
+++ random_game.py
@@ -1,15 +1,35 @@
 import random
 
-r = random.randint(1,10) ##先讓系統隨機建立一個數字
 count = 0 #猜的次數
 
+y = input("\n猜數字遊戲!!請輸入要猜的區間~第一個數字: ") # X = 使用者輸入數字
+z = input("\n請輸入要猜的區間~第二個數字: ")	
+y = int(y)
+z = int(z)
+
+	
+
+while y > z :
+	print("\n第二個數字不能比較小啦 =皿=")
+	y = input("\n重新輸入要猜的區間~第一個數字: ") # X = 使用者輸入數字
+	z = input("\n輸入要猜的區間~第二個數字: ")	
+	y = int(y)
+	z = int(z)
+
+r = random.randint(y,z) 
+
 while True:
-	x = input("猜數字遊戲!!請輸入1~10的整數: ") # X = 使用者輸入數字
+	
+	x = input("開始猜吧~~請輸入數字: ")
 	x = int(x)
+	
+
 	count += 1
 	if x == r:
 		print('\n答對了~~就是',r,'唷~~遊戲結束~~!!^O^~\n')
+		
 		break
+
 	elif x < r :
 		print('\n再大一點','你猜了',count,'次了~~\n')
 	elif x > r :
